972c67e1e538eabbb617cfbbda2b5901505470da
hai <pnghai@gmail.com>
2023-03-07 22:52:55 +0700

    change to deprecated body pix for eye detection

--- src/index.ts
+++ src/index.ts
@@ -7,44 +7,70 @@ import './scss/styles.scss';
 import $ from 'jquery';
 import * as tf from '@tensorflow/tfjs-core';
 import '@tensorflow/tfjs-backend-webgl';
+import { BodyPix, SemanticPartSegmentation } from '@tensorflow-models/body-pix';
+import * as bodyPix from '@tensorflow-models/body-pix';
 // import * as tfjsWasm from '@tensorflow/tfjs-backend-wasm';
 import * as bodySegmentation from '@tensorflow-models/body-segmentation';
 import { InputImage } from '@mediapipe/pose';
 import { drawKeypoints, drawSkeleton } from '@/js/utils';
-import { Segmentation } from '@tensorflow-models/body-segmentation/dist/shared/calculators/interfaces/common_interfaces';
 // Uncomment the line below if you want to use TensorFlow.js runtime.
 // import '@tensorflow/tfjs-converter';
 
+const rainbow = [
+  [110, 64, 170],
+  [143, 61, 178],
+  [178, 60, 178],
+  [210, 62, 167],
+  [238, 67, 149],
+  [255, 78, 125],
+  [255, 94, 99],
+  [255, 115, 75],
+  [255, 140, 56],
+  [239, 167, 47],
+  [217, 194, 49],
+  [194, 219, 64],
+  [175, 240, 91],
+  [135, 245, 87],
+  [96, 247, 96],
+  [64, 243, 115],
+  [40, 234, 141],
+  [28, 219, 169],
+  [26, 199, 194],
+  [33, 176, 213],
+  [47, 150, 224],
+  [65, 125, 224],
+  [84, 101, 214],
+  [99, 81, 195],
+];
 const canvasElement = document.getElementsByClassName(
   'output_canvas',
 )[0] as HTMLCanvasElement;
 const ctx = canvasElement.getContext('2d');
-let segmenter: bodySegmentation.BodySegmenter;
+let segmenter: BodyPix;
 async function setup() {
   // tfjsWasm.setWasmPaths(
   //   `https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-backend-wasm@${
   //     tfjsWasm.version_wasm}/dist/`);
   await tf.setBackend('webgl');
-  segmenter = await bodySegmentation.createSegmenter(
-    bodySegmentation.SupportedModels.BodyPix,
-    {
-      architecture: 'ResNet50',
-      outputStride: 16,
-      multiplier: 1.0,
-      quantBytes: 4,
-    },
-  );
+  segmenter = await bodyPix.load({
+    architecture: 'ResNet50',
+    outputStride: 16,
+    multiplier: 1.0,
+    quantBytes: 4,
+  });
 }
 
 function drawPoses(
-  personOrPersonPartSegmentation: Segmentation[],
+  personOrPersonPartSegmentation: SemanticPartSegmentation,
   flipHorizontally: boolean,
   ctx: CanvasRenderingContext2D | null,
 ) {
   if (Array.isArray(personOrPersonPartSegmentation)) {
+    console.log('personOrPersonPartSegmentation');
     personOrPersonPartSegmentation.forEach((personSegmentation) => {
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
+      console.log('pose');
       const pose = personSegmentation.pose;
       // if (flipHorizontally) {
       //   pose = bodySegmentation.flipPoseHorizontal(pose, personSegmentation.width);
@@ -52,21 +78,36 @@ function drawPoses(
       drawKeypoints(pose.keypoints, 0.1, ctx);
       drawSkeleton(pose.keypoints, 0.1, ctx);
     });
+  } else {
+    personOrPersonPartSegmentation.allPoses.forEach((pose) => {
+      if (flipHorizontally) {
+        pose = bodyPix.flipPoseHorizontal(
+          pose,
+          personOrPersonPartSegmentation.width,
+        );
+      }
+      drawKeypoints(pose.keypoints, 0.1, ctx);
+      drawSkeleton(pose.keypoints, 0.1, ctx);
+    });
   }
 }
 async function body_segment(image: InputImage) {
-  const segmentation = await segmenter.segmentPeople(image, {
-    flipHorizontal: false,
-    multiSegmentation: false,
-    segmentBodyParts: true,
-    // segmentationThreshold: STATE.visualization.foregroundThreshold
-  });
+  const segmentation = await segmenter.segmentPersonParts(
+    image,
+    //   , {
+    //   internalResolution: ,
+    //   segmentationThreshold: ,
+    //   maxDetections: ,
+    //   scoreThreshold: ,
+    //   nmsRadius: ,
+    // }
+  );
   // Convert the segmentation into a mask to darken the background.
-  const backgroundColor = { r: 0, g: 0, b: 0, a: 255 };
-  const coloredPartImage = await bodySegmentation.toColoredMask(
+  const coloredPartImage = await bodyPix.toColoredPartMask(
     segmentation,
-    bodySegmentation.bodyPixMaskValueToRainbowColor,
-    backgroundColor,
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    rainbow,
   );
   const opacity = 0.7;
   const flipHorizontal = false;
