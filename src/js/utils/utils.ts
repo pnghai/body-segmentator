3a71b129a609d6dc6639df650765297a270733de
hai <pnghai@gmail.com>
2023-03-27 16:50:02 +0700

    support middle line through body

--- src/js/utils/utils.ts
+++ src/js/utils/utils.ts
@@ -55,26 +55,63 @@ export function drawSkeleton(
   keypoints: Keypoint[],
   ctx: CanvasRenderingContext2D,
 ): void {
+  const mapping = poseDetection.util.getKeypointIndexByName(
+    poseDetection.SupportedModels.BlazePose,
+  );
+  const middleTop = middlePoint(
+    keypoints[mapping['left_shoulder']],
+    keypoints[mapping['right_shoulder']],
+    'middle_top',
+  );
+  middleTop.y =
+    Math.min(
+      keypoints[mapping['left_eye_inner']].y,
+      keypoints[mapping['right_eye_inner']].y,
+    ) * 0.8;
+  const middleBottom = middlePoint(
+    keypoints[mapping['left_hip']],
+    keypoints[mapping['right_hip']],
+    'middle_bottom',
+  );
+  middleBottom.y = Math.max(
+    keypoints[mapping['left_foot_index']].y,
+    keypoints[mapping['right_foot_index']].y,
+  );
+
+  ctx.fillStyle = 'Red';
+  ctx.strokeStyle = 'Red';
+  ctx.lineWidth = DEFAULT_LINE_WIDTH;
+  draw(middleTop, middleBottom, ctx);
+  draw(keypoints[mapping['left_knee']], keypoints[mapping['right_knee']], ctx);
   ctx.fillStyle = 'White';
   ctx.strokeStyle = 'White';
   ctx.lineWidth = DEFAULT_LINE_WIDTH;
-
   poseDetection.util
     .getAdjacentPairs(poseDetection.SupportedModels.BlazePose)
-    .forEach(([i, j]) => {
-      const kp1 = keypoints[i];
-      const kp2 = keypoints[j];
-
-      // If score is null, just show the keypoint.
-      const score1 = kp1.score != null ? kp1.score : 1;
-      const score2 = kp2.score != null ? kp2.score : 1;
-      const scoreThreshold = SCORE_THRESHOLD || 0;
-
-      if (score1 >= scoreThreshold && score2 >= scoreThreshold) {
-        ctx.beginPath();
-        ctx.moveTo(kp1.x, kp1.y);
-        ctx.lineTo(kp2.x, kp2.y);
-        ctx.stroke();
-      }
-    });
+    .forEach(([i, j]) => draw(keypoints[i], keypoints[j], ctx));
+}
+function draw(
+  kp1: Keypoint,
+  kp2: Keypoint,
+  ctx: CanvasRenderingContext2D,
+): void {
+  const scoreThreshold = SCORE_THRESHOLD || 0;
+  // If score is null, just show the keypoint.
+  const score1 = kp1.score != null ? kp1.score : 1;
+  const score2 = kp2.score != null ? kp2.score : 1;
+  if (score1 >= scoreThreshold && score2 >= scoreThreshold) {
+    ctx.beginPath();
+    ctx.moveTo(kp1.x, kp1.y);
+    ctx.lineTo(kp2.x, kp2.y);
+    ctx.stroke();
+  }
+}
+function middlePoint(kp1: Keypoint, kp2: Keypoint, name: string): Keypoint {
+  return {
+    x: (kp1.x + kp2.x) / 2,
+    y: (kp1.y + kp2.y) / 2,
+    // z: (kp1.z + kp2.z) / 2,
+    // score: (kp1.score + kp2.score) / 2,
+    name: name,
+  };
 }
